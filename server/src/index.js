40ac3061efa4233f88edbc52b948ee1c0c2c04f7
hpal <ahosea15@gmail.com>
2020-05-03 22:42:10 +0300

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.get('/', (req, res) => res.send('hello'))
+
+app.listen(port)
+
+console.log(`App running on port ${port}`)
