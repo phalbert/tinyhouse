19f422ff64fcbc629dbfe28a1ca67e069446b6b4
hpal <ahosea15@gmail.com>
2020-05-04 00:37:41 +0300

    added simple rest api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,25 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+import { listings } from './listings'
 
 const app = express()
 const port = 9000
 
+app.use(bodyParser.json())
+
+
 app.get('/', (_req, res) => res.send('hello'))
+
+app.get('/listings', (req, res) => {
+    return res.send(listings)
+})
+
+app.post('/listings/delete', (req, res) => {
+    const id: string = req.body.id
+    const filteredItems = listings.filter(item => item.id !== id)
+    return res.send(filteredItems)
+})
 
 app.listen(port)
 
